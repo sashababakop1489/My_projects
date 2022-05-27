53ca392fd4807c56f08a8609a9bd47ae9f38d990
sashababakop1489 <90720212+sashababakop1489@users.noreply.github.com>
2022-05-27 17:59:39 +0300

    Delete Football_Manager directory

--- Football_Manager/src/main/java/com/babakov/demo/facade/TeamFacade.java
+++
@@ -1,7 +0,0 @@
-package com.babakov.demo.facade;
-
-import com.babakov.demo.dto.team.TeamRequestDto;
-import com.babakov.demo.dto.team.TeamResponseDto;
-
-public interface TeamFacade extends BaseFacade<TeamRequestDto, TeamResponseDto>{
-}
