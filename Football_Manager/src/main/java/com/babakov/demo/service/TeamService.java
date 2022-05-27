10071c62f4c18c9a55c1d8efaf44943e9b5acbed
sashababakop1489 <90720212+sashababakop1489@users.noreply.github.com>
2022-05-27 23:30:20 +0300

    Delete Football_Manager directory

--- Football_Manager/src/main/java/com/babakov/demo/service/TeamService.java
+++
@@ -1,9 +0,0 @@
-package com.babakov.demo.service;
-
-import com.babakov.demo.models.Team;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface TeamService extends BaseService<Team>{
-
-}
