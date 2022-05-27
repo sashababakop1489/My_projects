1fa209841848d206e9e6447ede77cbe80dc0a455
sashababakop1489 <90720212+sashababakop1489@users.noreply.github.com>
2022-05-27 18:48:50 +0300

    Delete Football_Manager directory

--- Football_Manager/src/main/java/com/babakov/demo/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.babakov.demo.repository;
-
-import com.babakov.demo.models.Team;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamRepository extends BaseRepository<Team>{
-
-}
